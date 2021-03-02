22b115994ef211560e8e5f43913883e2f92f7489
airily-note <it66@qq.com>
2021-03-02 14:15:33 +0800

    分布式调度解决方案学习

---
+++ xxl-job-executor/xxl-job-executor-frameless/src/main/java/com/xxl/job/executor/frameless/config/FrameLessXxlJobConfig.java
@@ -0,0 +1,98 @@
+package com.xxl.job.executor.frameless.config;
+
+import com.xxl.job.executor.frameless.jobhandler.SampleXxlJob;
+import com.xxl.job.core.executor.impl.XxlJobSimpleExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author xuxueli 2018-10-31 19:05:43
+ */
+public class FrameLessXxlJobConfig {
+    private static Logger logger = LoggerFactory.getLogger(FrameLessXxlJobConfig.class);
+
+
+    private static FrameLessXxlJobConfig instance = new FrameLessXxlJobConfig();
+    public static FrameLessXxlJobConfig getInstance() {
+        return instance;
+    }
+
+
+    private XxlJobSimpleExecutor xxlJobExecutor = null;
+
+    /**
+     * init
+     */
+    public void initXxlJobExecutor() {
+
+        // load executor prop
+        Properties xxlJobProp = loadProperties("xxl-job-executor.properties");
+
+        // init executor
+        xxlJobExecutor = new XxlJobSimpleExecutor();
+        xxlJobExecutor.setAdminAddresses(xxlJobProp.getProperty("xxl.job.admin.addresses"));
+        xxlJobExecutor.setAccessToken(xxlJobProp.getProperty("xxl.job.accessToken"));
+        xxlJobExecutor.setAppname(xxlJobProp.getProperty("xxl.job.executor.appname"));
+        xxlJobExecutor.setAddress(xxlJobProp.getProperty("xxl.job.executor.address"));
+        xxlJobExecutor.setIp(xxlJobProp.getProperty("xxl.job.executor.ip"));
+        xxlJobExecutor.setPort(Integer.valueOf(xxlJobProp.getProperty("xxl.job.executor.port")));
+        xxlJobExecutor.setLogPath(xxlJobProp.getProperty("xxl.job.executor.logpath"));
+        xxlJobExecutor.setLogRetentionDays(Integer.valueOf(xxlJobProp.getProperty("xxl.job.executor.logretentiondays")));
+
+//        List<SampleXxlJob> jobList = new ArrayList<>(Arrays.asList(new SampleXxlJob()));
+        List<Object> jobList = new ArrayList<Object>();
+        jobList.add(new SampleXxlJob());
+        // registry job bean
+        xxlJobExecutor.setXxlJobBeanList(jobList);
+
+        // start executor
+        try {
+            xxlJobExecutor.start();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * destory
+     */
+    public void destoryXxlJobExecutor() {
+        if (xxlJobExecutor != null) {
+            xxlJobExecutor.destroy();
+        }
+    }
+
+
+    public static Properties loadProperties(String propertyFileName) {
+        InputStreamReader in = null;
+        try {
+            ClassLoader loder = Thread.currentThread().getContextClassLoader();
+
+            in = new InputStreamReader(loder.getResourceAsStream(propertyFileName), "UTF-8");;
+            if (in != null) {
+                Properties prop = new Properties();
+                prop.load(in);
+                return prop;
+            }
+        } catch (IOException e) {
+            logger.error("load {} error!", propertyFileName);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    logger.error("close {} error!", propertyFileName);
+                }
+            }
+        }
+        return null;
+    }
+
+}
---
+++ xxl-job-executor/xxl-job-executor-frameless/src/test/java/com/xxl/job/executor/frameless/test/FramelessApplicationTest.java
@@ -0,0 +1,12 @@
+package com.xxl.job.executor.frameless.test;
+
+import org.junit.jupiter.api.Test;
+
+public class FramelessApplicationTest {
+
+    @Test
+    public void test(){
+        System.out.println("111");
+    }
+
+}
